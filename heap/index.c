e84f8c91126db7a9854ac3ff6e8ca8ec0ec2fe40
baconYao <bacon735392@gmail.com>
2017-08-24 05:56:19 +0800

    Add head DS

--- heap/index.c
+++ heap/index.c
@@ -36,7 +36,97 @@ int min(int nb1, int nb2) {
   return nb1 < nb2 ? nb1 : nb2;
 }
 
+void insert_max_heap(heap *h, int value) {
+  h -> arr[++h -> size] = value;
+  int child_index = h -> size;
+  while(child_index > 1 && h -> arr[child_index] > h -> arr[child_index / 2]) {
+    swap(&h -> arr[child_index], &h -> arr[child_index / 2]);
+  }
+}
+
+void insert_min_heap(heap *h, int value) {
+  h -> arr[++h -> size] = value;
+  int child_index = h -> size;
+  while(child_index > 1 && h -> arr[child_index] < h -> arr[child_index / 2]) {
+    swap(&h -> arr[child_index], &h -> arr[child_index / 2]);
+  }
+}
+
+int extract_max_heap(heap *h) {
+  if(empty_heap(h)) {
+    printf("Heap is empty!");
+    return 0;
+  }
+
+  int value = h -> arr[1];
+  h -> arr[1] = h -> arr[h -> size];
+  h -> size--;
+  int parentIndex = 1;
+  int leftChildIndex, rightChildIndex;
+
+  do {
+    leftChildIndex = parentIndex * 2;
+    rightChildIndex = parentIndex * 2 + 1;
+    if(h -> arr[parentIndex] > max(h -> arr[leftChildIndex], h -> arr[rightChildIndex])) {
+      break;
+    }
+    
+    if(h -> size == leftChildIndex || h -> arr[leftChildIndex] > h -> arr[rightChildIndex]) {
+      swap(&h -> arr[parentIndex], &h -> arr[leftChildIndex]);
+      parentIndex = leftChildIndex;
+    }
+    else {
+      swap(&h -> arr[parentIndex], &h -> arr[rightChildIndex]);
+      parentIndex = rightChildIndex;
+    }
+
+  } while(parentIndex < h -> size);
+  return value;
+}
+
+int extract_min_heap(heap *h) {
+  if(empty_heap(h)) {
+    printf("Heap is empty!");
+    return 0;
+  }
+
+  int value = h -> arr[1];
+  h -> arr[1] = h -> arr[h -> size];
+  h -> size--;
+  int parentIndex = 1;
+  int leftChildIndex, rightChildIndex;
+
+  do {
+    leftChildIndex = parentIndex * 2;
+    rightChildIndex = parentIndex * 2 + 1;
+    if(h -> arr[parentIndex] < min(h -> arr[leftChildIndex], h -> arr[rightChildIndex])) {
+      break;
+    }
+    
+    if(h -> size == leftChildIndex || h -> arr[leftChildIndex] < h -> arr[rightChildIndex]) {
+      swap(&h -> arr[parentIndex], &h -> arr[leftChildIndex]);
+      parentIndex = leftChildIndex;
+    }
+    else {
+      swap(&h -> arr[parentIndex], &h -> arr[rightChildIndex]);
+      parentIndex = rightChildIndex;
+    }
+
+  } while(parentIndex < h -> size);
+  return value;
+}
+
+
 int main(int argc, char **argv ) {
+  heap *h = create_heap(100);
+  insert_min_heap(h, 6);
+  insert_min_heap(h, 3);
+  insert_min_heap(h, 9);
+  insert_min_heap(h, 5);
+  insert_min_heap(h, 3);
+  while(!empty_heap(h)) {
+    printf("%d\n", extract_min_heap(h));
+  }
 
   return 0;
 }
